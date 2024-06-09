7ed3d76cdc92a021753f24da154a74e874499456
adamhlomgern <146996877+adamhlomgern@users.noreply.github.com>
2024-06-09 21:22:20 +0200

    v3

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # Random number game
 
+
+# Imports
+import random
+from art import logo
+
+
+# Variables
+difficulty =
+
+
+# Functions
+def start_game():
+    print(logo)
+    print("Welcome to the random number game!")
+    dicciculty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if dicciculty == easy
